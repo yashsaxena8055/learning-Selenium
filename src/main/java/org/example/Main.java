a5a9a5516afb0641dafae3c462f7451127a9852f
yashsaxena8055 <yash.saxena@credable.in>
2022-03-02 11:02:50 +0530

    driver manager is being added

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,51 +1,39 @@
 package org.example;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxBinary;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.firefox.FirefoxOptions;
-
-import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
 
-//        FirefoxOptions options=new FirefoxOptions();
-//        options.setBinary("/usr/bin/firefox");
-//
-
-        WebDriver driver=firefoxDriver();
-//        WebDriver driver = new ChromeDriver();
+//        WebDriver driver=firefoxDriver();
+        WebDriver driver = chromeDriver();
         driver.get("http://google.com");
 
-//        List<WebElement> s= driver.findElements(By.name("q"));
-//
-//        WebElement searchBox= s.get(0);
 
         WebElement searchBox=driver.findElement(By.name("q"));
-
-
         searchBox.click();
         searchBox.sendKeys("selenium");
 
         searchBox.sendKeys(Keys.ENTER);
 
-        List<WebElement> link = driver.findElements(By.className("yuRUbf"));
-        WebElement element=link.get(2);
-        element.click();
+
 
 
     }
     public static WebDriver firefoxDriver()
     {
-             return new FirefoxDriver();
+        WebDriverManager.firefoxdriver().setup();
+        return new FirefoxDriver();
     }
    public static WebDriver chromeDriver(){
+       WebDriverManager.chromedriver().setup();
         return new ChromeDriver();
    }
 
