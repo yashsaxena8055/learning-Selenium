35b6c12a4ef405a62981b3ae79bfda544f5a0330
yashsaxena8055 <yash.saxena@credable.in>
2022-03-04 11:10:32 +0530

    Adding Another Test Case!!

--- src/test/java/org/example/SeleniumWebsiteTest.java
+++ src/test/java/org/example/SeleniumWebsiteTest.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -32,5 +33,26 @@ public class SeleniumWebsiteTest {
         driver.quit();
 
 
+    }
+    @org.junit.Test
+    public void test_selenium_dot_dev_blog2017() {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver=new ChromeDriver();
+
+        driver.get("https://selenium.dev");
+
+        WebElement blog = driver.findElement(By.linkText("Blog"));
+        blog.click();
+
+        WebElement link2016 = driver.findElement(By.id("m-blog2017"));
+        link2016.click();
+
+        WebElement element = driver.findElement(By.linkText("Fall Selenium Conf, Save the Date & Call for Speakers!"));
+        assertEquals("Fall Selenium Conf, Save the Date & Call for Speakers!", element.getText());
+
+        String title = driver.getTitle();
+        assertEquals("Blog Posts - 2017 | Selenium", title);
+
+        driver.quit();
     }
 }
