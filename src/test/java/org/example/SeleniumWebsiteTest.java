429f91080cb3507de99ad5fd9972b501efba9949
yashsaxena8055 <yash.saxena@credable.in>
2022-03-04 11:25:03 +0530

    Adding @beforeclass @before ,@after

--- src/test/java/org/example/SeleniumWebsiteTest.java
+++ src/test/java/org/example/SeleniumWebsiteTest.java
@@ -1,58 +1,58 @@
 package org.example;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 import static org.junit.Assert.assertEquals;
 
 public class SeleniumWebsiteTest {
 
-    @org.junit.Test
-    public  void  test_selenium_dot_div_blog()
-    {
-        WebDriverManager.chromedriver().setup();
-        WebDriver driver=new ChromeDriver();
+    private WebDriver driver;
 
-        driver.get("https://www.selenium.dev/");
+    @BeforeClass
+    public static void init() {
+        WebDriverManager.firefoxdriver().setup();
+    }
 
-        WebElement searchBox=driver.findElement(By.linkText("Blog"));
-        searchBox.click();
-        searchBox=driver.findElement(By.id("m-blog2016-li"));
-        searchBox.click();
-        searchBox=driver.findElement(By.linkText("Fall Selenium Conf, Save the Date & Call for Speakers!"));
-        assertEquals("Fall Selenium Conf, Save the Date & Call for Speakers!",searchBox.getText());
+    @Before
+    public void setup() {
+        driver = new FirefoxDriver();
+        driver.get("https://selenium.dev");
+        WebElement blog = driver.findElement(By.linkText("Blog"));
+        blog.click();
+    }
+
+    @After
+    public void closeBrowser() {
+        driver.quit();
+    }
+
+    @Test
+    public void test_selenium_dot_dev_blog_2016_page() {
+        WebElement link2016 = driver.findElement(By.id("m-blog2016"));
+        link2016.click();
+
+        WebElement element = driver.findElement(By.linkText("Fall Selenium Conf, Save the Date & Call for Speakers!"));
+        assertEquals("Fall Selenium Conf, Save the Date & Call for Speakers!", element.getText());
 
         String title = driver.getTitle();
         assertEquals("Blog Posts - 2016 | Selenium", title);
-
-        driver.quit();
-
-
     }
-    @org.junit.Test
-    public void test_selenium_dot_dev_blog2017() {
-        WebDriverManager.chromedriver().setup();
-        WebDriver driver=new ChromeDriver();
 
-        driver.get("https://selenium.dev");
-
-        WebElement blog = driver.findElement(By.linkText("Blog"));
-        blog.click();
-
-        WebElement link2016 = driver.findElement(By.id("m-blog2017"));
-        link2016.click();
-
-        WebElement element = driver.findElement(By.linkText("Hacking with IntelliJ"));
-        assertEquals("Hacking with IntelliJ", element.getText());
+    @Test
+    public void test_selenium_dot_dev_blog_2017_page() {
+        WebElement link2017 = driver.findElement(By.id("m-blog2017"));
+        link2017.click();
 
         String title = driver.getTitle();
         assertEquals("Blog Posts - 2017 | Selenium", title);
-
-        driver.quit();
     }
+
 }
